f0745f59844ff93421d4d9ed47b458a7c21cf2a8
JunHyen Mun <impelfin@gmail.com>
2023-01-13 14:13:36 +0900

    nw

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function FirstPost() {
   return (
     <>
-        <h1>First Post</h1>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <h1>First Post!</h1>
         <h2>
             <Link href="/">
                 Home
