bd8584fc9bdd5354d4e1c9a79aec3157429c5107
JunHyen Mun <impelfin@gmail.com>
2023-01-17 15:33:31 +0900

    aas

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -1,19 +1,24 @@
-import Head from 'next/head'
-import Axios from 'axios';
-import { useEffect, useState } from 'react';
-import styles from '../styles/Home.module.css'
-import ItemList from '../src/component/ItemList';
-import { Divider, Header } from 'semantic-ui-react';
+import Axios from "axios";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { Divider, Header } from "semantic-ui-react";
+import ItemList from "../src/component/ItemList";
+import styles from "../styles/Home.module.css";
+import { Loader } from "semantic-ui-react";
 
 export default function Home() {
   const [list, setList] = useState([]);
-  const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+  const [isLoading, setIsLoading] = useState(true); 
+
+  const API_URL =
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
   function getData() {
     Axios.get(API_URL).then((res) => {
       console.log(res.data);
       setList(res.data);
-    })
+      setIsLoading(false);
+    });
   }
 
   useEffect(() => {
@@ -23,14 +28,29 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title> Home | IMP  </title>
+        <title>HOME | IMP</title>
       </Head>
-      <Header as="h3" style={{ paddingTop: 40 }}>베스트 상품</Header>
-      <Divider />
-      <ItemList list={list.slice(0, 9)} />
-      <Header as="h3" style={{ paddingTop: 40 }}>신상품</Header>
-      <Divider />
-      <ItemList list={list.slice(9)} />
+      {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            베스트 상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(0, 9)} />
+          <Header as="h3" style={{ paddingTop: 40 }}>
+            신상품
+          </Header>
+          <Divider />
+          <ItemList list={list.slice(9)} />
+        </>
+      )}
     </div>
-  )
+  );
 }
--- nextjs-tutorial/src/component/Top.js
+++ nextjs-tutorial/src/component/Top.js
@@ -2,19 +2,19 @@ import { Header } from "semantic-ui-react";
 import Gnb from "./Gnb";
 
 export default function Top() {
-    return ( 
-        <div> 
-            <div style={{ display: "felx", paddingTop : 20 }}>
-                <div style={{ flex: "100px 0 0" }}>
-                    <img 
-                        src="/images/imp_small.jpg" 
-                        width="100" height="90" 
-                        alt="logo" 
-                        sytle={{ display: "block" }}
-                    />
-                </div>
-            </div>
-            <Gnb />
+  return (
+    <div>
+      <div style={{ display: "flex", paddingTop: 20 }}>
+        <div style={{ flex: "100px 0 0" }}>
+          <img
+            src="/images/imp_small.jpg"
+            alt="logo"
+            style={{ display: "block", width: 80 }}
+          />
         </div>
-    )
+        <Header as="h1"> IMP </Header>
+      </div>
+      <Gnb />
+    </div>
+  );
 }
